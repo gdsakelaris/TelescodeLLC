06ab46505a6b461e513ecb24f32f6579c720e4f2
gdsakelaris <gsakelar@depaul.edu>
2023-07-18 02:21:01 -0500

    i

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import logo from './logo.svg';
+import logo from './logo2.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <code>- Telescode LLC - </code>
+          <code>Telescode LLC</code>
         </p>
-        <a
+        <br /><br />
+        <img src={logo} className="App-logo" alt="logo" />
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
