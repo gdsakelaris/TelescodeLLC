95206b46052495a8fb8838400522bb2c908f9c41
gdsakelaris <gsakelar@depaul.edu>
2023-07-18 01:08:02 -0500

    t

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          I was here - yyyEdit <code>src/App.js</code> and save to reload.
+          <code>Telescode LLC</code>
         </p>
         <a
           className="App-link"
