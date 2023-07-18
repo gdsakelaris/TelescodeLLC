cb29f6ceb7453b5cbbef96d3879ba773a26bdf9f
gdsakelaris <gsakelar@depaul.edu>
2023-07-18 01:28:04 -0500

    t

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <code>Telescode LLC</code>
+          <code>- Telescode LLC - </code>
         </p>
         <a
           className="App-link"
