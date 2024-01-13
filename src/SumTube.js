99719862e28c489bb6658b227827b681d7932cb9
gdsakelaris <gdsakelaris@gmail.com>
2024-01-13 12:14:43 -0600

    SumTube project

---
+++ src/SumTube.js
@@ -0,0 +1,28 @@
+import logo from './logo2.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>
+          <code>SumTube</code>
+        </h1>
+      </header>
+      <a
+        className="App-link"
+        href="http://3.218.77.172/accounts/login/?next=/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Go to SumTube
+      </a>
+      <div className="App-footer">
+      <p>© 2023 Telescode LLC. All rights reserved.</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import SumTube from './SumTube';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/sumtube" element={<SumTube />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
 
