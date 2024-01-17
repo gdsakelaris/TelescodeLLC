477e70a329881292ea394a36093cbbc729a57205
gdsakelaris <gdsakelaris@gmail.com>
2024-01-17 01:27:11 -0600

    RESOLVED

--- src/index.js
+++ src/index.js
@@ -1,19 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import SumTube from './SumTube';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/sumtube" element={<SumTube />} />
-      </Routes>
-    </Router>
+    <App />
   </React.StrictMode>
 );
 
